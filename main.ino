78918e48b97bed2980bde29262f4d7bd968f66e5
Matheus Tavares <matheustavarestrindade@hotmail.com>
2022-12-20 18:32:37 -0300

    Add files via upload

--- main.ino
+++ main.ino
@@ -5,8 +5,8 @@
 #include <SPI.h>
 #include <WiFi.h>
 
-char ssid[] = "Batttery Manager Interface";
-char pass[] = "manager123";
+char ssid[] = "Rede trindade repetidor";
+char pass[] = "";
 
 int status = WL_IDLE_STATUS;
 WiFiServer server(80);
@@ -14,21 +14,20 @@ WiFiServer server(80);
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1
+
+#define ADC_PIN 34
+#define RELAY_PIN 14
+#define SCREEN_SDA 21
+#define SCREEN_SCL 22
+
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 
-int batteryADCVoltage = 0;
+int battery_adc_voltage = 0;
 float voltage_value;
 
-const int ADC_PIN = 34;
-const int RELAY_PIN = 14;
-
-const int SCREEN_SDA = 21;
-const int SCREEN_SCL = 22;
-
 String header;
-
-int mode = 1;
+int mode = -1;
 
 void setup() {
 
@@ -41,36 +40,76 @@ void setup() {
   }
   display.display();
   // attempt to connect to Wifi network:
-  WiFi.softAP(ssid, pass);
+  WiFi.mode(WIFI_STA); 
+  WiFi.begin(ssid, pass);
+
+  Serial.println("\nConnecting");
+
+  while(WiFi.status() != WL_CONNECTED){
+      Serial.print(".");
+      delay(100);
+  }
+
+  Serial.println("\nConnected to the WiFi network");
+  Serial.print("Local ESP32 IP: ");
+  Serial.println(WiFi.localIP());
+
   server.begin();
 
   // put your setup code here, to run once:
   pinMode(RELAY_PIN, OUTPUT);
   delay(1000);
+
 }
 
 long currentTime = millis();
+float last_battery_values[12] = {0,0,0,0,0,0,0,0,0,0,0,0};
+int last_battery_value_index = 0;
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (currentTime + 2000 < millis()) {
-    currentTime = millis();
-    batteryADCVoltage = analogRead(ADC_PIN);
-    Serial.print("Battery Voltage: ");
-    voltage_value = (batteryADCVoltage * 3.45 * 11) / (4095);
-    Serial.println(voltage_value);
-    if (voltage_value < 22 && mode != LOW) {
-      mode = LOW;
-      digitalWrite(RELAY_PIN, mode);
+
+  if(currentTime + 5000 > millis()) { handleWifi();  return; }
+  currentTime = millis();
+  
+  calculateBatteryVoltage();
+
+  if (voltage_value < 22 && mode != 0) {
+      mode = 0;
       Serial.println("Battery is low, turning off.");
-    } else if (voltage_value > 26 && mode != HIGH) {
-      mode = HIGH;
-      Serial.println("Battery is back online! Turning on.");
-      digitalWrite(RELAY_PIN, mode);
-    }
-    drawInfo();
+      digitalWrite(RELAY_PIN, LOW);
+  } else if (voltage_value > 25 && mode != 1) {
+    mode = 1;
+    Serial.println("Battery is back online! Turning on.");
+    digitalWrite(RELAY_PIN, HIGH);
   }
+
+  drawInfo();
   handleWifi();
+}
+
+void calculateBatteryVoltage() {
+  battery_adc_voltage = analogRead(ADC_PIN);
+  Serial.print("Battery Voltage: ");
+  float read_voltage = (battery_adc_voltage * 3.47 * 11) / (4095);
+  Serial.println(read_voltage);
+
+  last_battery_values[last_battery_value_index] = read_voltage;
+  if(last_battery_value_index == 11) last_battery_value_index = 0;
+  else last_battery_value_index++;
+
+  int i;
+  float total = 0;
+  Serial.print("Last Values: ");
+  for(i = 0; i < 12; i++){
+    total += last_battery_values[i];
+    Serial.print(last_battery_values[i]);
+    Serial.print(", ");
+  }
+  voltage_value = (total / 12);
+  Serial.print("Battery calculated Voltage 60s: ");
+  Serial.println(voltage_value);
+
+
 }
 
 void handleWifi() {
@@ -146,7 +185,7 @@ void drawInfo() {
   display.setCursor(0, 40);
   display.write("Soft AP IP");
   display.setCursor(0, 50);
-  display.write(WiFi.softAPIP().toString().c_str());
+  display.write(WiFi.localIP().toString().c_str());
 
   display.display();
   delay(2000);
